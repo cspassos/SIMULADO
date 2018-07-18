e29aaf34853974928733af87cdd6f5cd2f641df5
cspassos <cspassos@latam.stefanini.com>
2018-07-18 10:54:07 -0300

    Layout inicial responsivo

--- simulado_front/src/app/app.module.ts
+++ simulado_front/src/app/app.module.ts
@@ -3,26 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { routes } from './app.routes';
 import { UserService } from './services/user.service';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { UserNewComponent } from './components/user-new/user-new.component';
-import { UserListComponent } from './components/user-list/user-list.component';
 import { DialogService } from './dialog.service';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent,
     FooterComponent,
     HomeComponent,
-    UserNewComponent,
-    UserListComponent
+    MenuComponent
   ],
   imports: [
     BrowserModule,
--- simulado_front/src/app/app.routes.ts
+++ simulado_front/src/app/app.routes.ts
@@ -2,14 +2,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 import { HomeComponent } from './components/home/home.component';
-import { UserNewComponent } from './components/user-new/user-new.component';
-import { UserListComponent } from './components/user-list/user-list.component';
 
 export const ROUTES: Routes = [
-    { path : '', component: HomeComponent}, //rota inicial
-    { path : 'user-new', component: UserNewComponent},
-    { path : 'user-new/:id', component: UserNewComponent},
-    { path : 'user-list', component: UserListComponent},
+    { path : '', component: HomeComponent} //rota inicial
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
